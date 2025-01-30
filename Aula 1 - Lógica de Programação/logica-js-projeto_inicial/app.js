0e897d8d2e1fc6a83bc7d4450da798d79473dc54
Gabriel Teófilo <gabriel22072002@gmail.com>
2025-01-30 19:04:49 -0300

    Class 1 - 30/01/2025

--- Aula 1 - Lógica de Programação/logica-js-projeto_inicial/app.js
+++ Aula 1 - Lógica de Programação/logica-js-projeto_inicial/app.js
@@ -3,7 +3,7 @@ alert("Boas Vindas ao Curso de Lógica de Programação");
 
 //Variáveis
 let numeroSecreto = 33;
-
+let tentativas = 1;
 //input do usuário e armazenamento
 let num_chute = prompt("Digite um número entre 1 e 50");
 
@@ -13,5 +13,40 @@ if (numeroSecreto == num_chute) {
 }
 // outra possibilidade seria `Parabens, você acertou o número secreto ${numeroSecreto}!`
 else{
-    alert("Que pena, você errou o número secreto!\n Tente novamente!");
+    if(numeroSecreto>num_chute){
+    alert("Que pena, você errou o número secreto eh maior!\n Tente novamente!");
+    }
+    else{
+    alert("Que pena, você errou o número secreto eh menor!\n Tente novamente!");             
+    }
+    tentativas++;
 }
+
+//loops - while
+while(numeroSecreto != num_chute){
+    chute = prompt("Digite um novo número entre 1 e 50");
+    num_chute = prompt("Digite um número entre 1 e 50");
+    if (numeroSecreto == num_chute) {
+        break;
+        }
+    else{
+        if(numeroSecreto>num_chute){
+        alert("Que pena, você errou o número secreto eh maior!\n Tente novamente!");
+        }
+        else{
+        alert("Que pena, você errou o número secreto eh menor!\n Tente novamente!");             
+        }
+        tentativas++;
+    }
+}
+if(tentativas==1)
+    alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} chute!`);
+else
+    alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} chutes!`);
+    
+// operator ternário - ? é uma outra possibilidade de fazer a mesma coisa
+let palavraTentativas = tentativas == 1 ? "tentativa" : "tentativas";
+
+// math random() - gera um número aleatório
+// math round() - arredonda o número
+numeroSecreto = Math.round(Math.random() * 50);
